92cf801cc4ab46517ff005e908472656224cce37
imeankenshin <omurakenshin@gmail.com>
2024-03-15 07:35:31 +0900

    From alacritty to weaterm

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,31 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+-- font
+
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.font_size = 14
+-- window
+
+local x = 16 * 6
+local y = 16 * 2
+
+config.window_padding = {
+  left = x,
+  right = x,
+  top = y,
+  bottom = y,
+}
+config.color_scheme = 'Tokyo Night'
+--- keybindings
+
+config.keys = {
+  {key="p", mods="CMD", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+  {key="o", mods="CMD", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+  {key="l", mods="CMD", action=wezterm.action{ActivatePaneDirection="Right"}},
+  {key="h", mods="CMD", action=wezterm.action{ActivatePaneDirection="Left"}},
+}
+
+-- reutrn
+
+return config
+
