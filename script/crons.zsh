05ac1f7ff3ab5dfc2bc2e01c54f3f65abe7e72e1
imeankenshin <omurakenshin@gmail.com>
2024-03-15 07:35:53 +0900

    Update crons.zsh

--- script/crons.zsh
+++ script/crons.zsh
@@ -1,8 +1,11 @@
 # _cron.zsh is a file that contains all the cron jobs that are used when the shell is loaded
 # A cron job to ask the user to install missing plugins if there are any
 if ! zplug check --verbose; then
-  printf "Install missing plugins? [y/N]: "
-  if read -q; then
-    echo; zplug install
-  fi
+  gum confirm "Install missing plugins?" && zplug install
 fi
+
+# A cron job to ask the user to update dotfiles repository if there are any
+if [[ -n $(git --git-dir $DOTFILES status -s) ]]; then
+  gum confirm "Update dotfiles repository?" && git pull
+fi
+
